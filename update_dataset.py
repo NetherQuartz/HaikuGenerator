fdd8eb97cb7b3a5fdb22f06f5e0f11158e8eeb85
NetherQuartz <vd-larkin@ya.ru>
2021-01-21 23:57:42 +0300

    Improve parser

--- update_dataset.py
+++ update_dataset.py
@@ -4,25 +4,34 @@ from pprint import pprint
 from bs4 import BeautifulSoup
 import re
 
-l = []
-off = 0
-count = 50
-while off < 300:
+links = []
+offset = 0
+load_batch = 50
+got_len = load_batch
+
+while got_len == load_batch:
     r = requests.get("https://api.dtf.ru/v1.8/user/157777/entries",
                      headers={
                          'User-Agent': "hokku-app/1.0 (Mi Notebook Pro; Windows/10; ru; 1980x1080)",
                      },
                      params={
-                         "count": str(count),
-                         "offset": str(off)
+                         "count": str(load_batch),
+                         "offset": str(offset)
                      }).json()
-    l += r["result"]
-    off += count
+    links += r["result"]
+    print(len(r['result']))
+    got_len = len(r["result"])
+    offset += got_len
     time.sleep(0.4)
 
-posts = []
-texts = []
-for e in l:
+pprint(links[0]["dateRFC"])
+pprint(links[-1]["dateRFC"])
+print(len(links))
+
+haikus = []
+start = 0
+for i, e in enumerate(links[start:]):
+    print(f"\n{i + start}/{len(links)}")
     r = requests.get("https://api.dtf.ru/v1.8/entry/locate",
                      headers={
                          'User-Agent': "hokku-app/1.0 (Mi Notebook Pro; Windows/10; ru; 1980x1080)",
@@ -33,23 +42,43 @@ for e in l:
     html = r["result"]["entryContent"]["html"]
     soup = BeautifulSoup(html, 'lxml')
     div = soup.select_one("div.content")
+    er = str(div.text)
+
+    div1 = div.select_one(".block-incut__text")
+
+    div2 = div.select("div.l-island-a")
+
+    div3 = div.select_one("div.block-quote__text")
+
+    children = []
+    if div1 is not None:
+        children = list(div1.children)
+    elif div3 is not None:
+        children = list(div3.children)
+    elif len(div2) > 0:
+        for c in div2:
+            children += list(c.children)
+    else:
+        children = None
+
+    st = ""
+
     try:
-        soup.select_one("textarea").decompose()
+        for c in children:
+            strings = re.split(r"<br\s?/?>", str(c))
+            strings = [re.sub(r"<[^<>]+>", "", s).strip() for s in strings]
+            strings = list(filter(lambda x: x != "" and re.match(r"^\s*$", x) is None, strings))
+            if len(strings) > 0:
+                st += "\n" if st != "" else ""
+                st += "\n".join(strings)
+
+        if len(st.split("\n")) < 3:
+            raise Exception
+        haikus.append(st)
+        print(st, "\n")
     except:
-        pass
-    div.select_one("div.l-island-a").decompose()
-
-    pat = r"([A-Za-zа-яА-Я0-9ёЁ.,\-\":?!—]+)"
-
-    texts.append(" ".join(re.findall(pat, div.text)))
-    posts.append(div.text)
+        print("ERROR:", f"{er.strip()}")
 
     time.sleep(0.4)
 
-pprint(texts)
-f = open("posts.txt", 'w', encoding="utf8")
-f.write("\n".join(posts))
-f.close()
-f = open("texts.txt", 'w', encoding="utf8")
-f.write("\n".join(texts))
-f.close()
+print(f"Without errors: {len(haikus)}/{len(links)}")
